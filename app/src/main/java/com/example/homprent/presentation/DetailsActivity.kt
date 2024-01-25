5a0b7f0311ac55d31a28612c7c54e5b519399308
RootHex200 <sabiturrahman60@gmail.com>
2024-01-25 20:31:18 +0600

    initial commit

---
+++ app/src/main/java/com/example/homprent/presentation/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homprent.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homprent.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
